40055d123585e2ea1806da8d7f00c111e908caff
jiangzhesheng <296392014@qq.com>
2020-12-02 12:22:30 +0800

    initial commit

--- algo.py
+++ algo.py
@@ -6,6 +6,7 @@
 import sys
 import myclass
 import time
+
 def dp_algo(word1:str,word2:str)->int:
     n = len(word1)
     m = len(word2)
@@ -26,7 +27,7 @@ def dp_algo(word1:str,word2:str)->int:
             down = D[i][j - 1] + 1
             left_down = D[i - 1][j - 1]
             if word1[i - 1] != word2[j - 1]:
-                left_down += 2
+                left_down += 1
             D[i][j] = min(left, down, left_down)
     return D[n][m]
 
@@ -52,7 +53,16 @@ def ond_algo(A:str,B:str)->int:
             if x>=n and y>=m:
                 return d
 
-def my_ond_algo(A:str,B:str)->int:
+class Point:
+    def __init__(self,x,k) -> None:
+        self.x=x
+        self.k=k
+        self.y=x-k
+
+    def __str__(self) -> str:
+        return '(%s,%s)' % (self.x,self.y)
+
+def my_ond_algo_version0(A:str, B:str)->[[]]:
     n=len(A)
     m=len(B)
     max_value=m+n
@@ -64,18 +74,81 @@ def my_ond_algo(A:str,B:str)->int:
     for d in range(1,max_value):
         tmp_v=[-1]*(2*d+1)
         for k in range(-d,d+2,2):
-            if k==-d or k!=d and v[d-1][k+d]<v[d-1][k+d+2]:
+            if k==-d:
                 x=v[d-1][k+d]#竖着走
+            elif k==d:
+                x=v[d-1][k+d-2]+1#横着走
             else:
-                x=v[d-1][k+d+2]+1#横着走
+                p1 = Point(x=v[d - 1][k + d], k=k+1)#竖着走的点
+                p2 = Point(x=v[d - 1][k + d - 2], k=k-1)#横着走的点
+                if p1.x>p2.x:
+                    x = v[d - 1][k + d]
+                else:
+                    x = v[d - 1][k + d - 2] + 1
             y=x-k
             while x<n and y<m and A[x]==B[y]:
                 x,y=x+1,y+1
             tmp_v[k+d]=x
             if x>=n and y>=m:
-                print(v)
-                return d
+                return v
         v.append(tmp_v)
+
+def list_get(index:int, list:[], default=None):
+    return list[index] if 0<= index < len(list) else default
+
+def my_ond_algo_version1(A:str, B:str)->[[]]:
+    n=len(A)
+    m=len(B)
+    max_value=m+n
+    v=[]
+    x,y=0,0
+    while x < n and y < m and A[x] == B[y]:
+        x, y = x + 1, y + 1
+    v.append([x])
+    for d in range(1,max_value):
+        tmp_v=[-1]*(2*d+1)
+        for k in range(-d,d+1,1):
+            x1=list_get(list=v[d-1],index=k+d-2,default=-1)
+            x2=list_get(list=v[d-1],index=k+d-1,default=-1)
+            x3=list_get(list=v[d-1],index=k+d,default=-1)
+            p1 = Point(x=x1, k=k-1)#横着走的点
+            p2= Point(x=x2,k=k)#substitution
+            p3 = Point(x=x3, k=k+1)#竖着走的点
+            x=max([p1.x+1,p2.x+1,p3.x])
+            y=x-k
+            while x<n and y<m and A[x]==B[y]:
+                x,y=x+1,y+1
+            tmp_v[k+d]=x
+            if x>=n and y>=m:
+                v.append(tmp_v)
+                return v
+        v.append(tmp_v)
+
+
+def get_edit_distance(v:[[]]):
+    return len(v)-1
+
+def get_edit_script(v:[[]])->[]:
+    script=[]
+    d=len(v)-1
+    k=v[-1].index(-1)-d-1 if -1 in v[-1] else d
+    for d in range(len(v)-1,0,-1):
+        x1 = list_get(list=v[d - 1], index=k + d - 2, default=-1)
+        x2 = list_get(list=v[d - 1], index=k + d - 1, default=-1)
+        x3 = list_get(list=v[d - 1], index=k + d, default=-1)
+        p1 = Point(x=x1, k=k - 1)  # 横着走的点
+        p2 = Point(x=x2, k=k)  # substitution
+        p3 = Point(x=x3, k=k + 1)  # 竖着走的点
+        p_list=[(p1.x+1,p1),(p2.x+1,p2),(p3.x,p3)]
+        p_list.sort(key=lambda p:p[0])
+        if p_list[-1][1]==p1:
+            script.insert(0,(p_list[-1][1].x,'D'))
+        elif p_list[-1][1]==p2:
+            script.insert(0,(p_list[-1][1].x,'S'+str(p_list[-1][1].y)))
+        elif p_list[-1][1]==p3:
+            script.insert(0,(p_list[-1][1].x,'I'+str(p_list[-1][1].y)))
+        k=p_list[-1][1].k
+    return script
 
 def main(argv):
     queryfile = myclass.Fastafile('test_data/query')
--- test_algo.py
+++ test_algo.py
@@ -18,15 +18,19 @@ class Test(TestCase):
     def test_my_ond_algo(self):
         A = 'ABCABBA'
         B = 'CBABAC'
-        ans = 5
-        ond_ans = algo.my_ond_algo(A, B)
+        ans = 4
+        ond_v = algo.my_ond_algo_version1(A, B)
+        ond_ans=algo.get_edit_distance(ond_v)
+        script=algo.get_edit_script(ond_v)
+        print(script)#[(0, 'S0'), (2, 'D'), (5, 'D'), (7, 'I5')]
         self.assertEqual(ond_ans, ans, 'ond_ans is %d but ans is ' % ond_ans + str(ans))
     def test_ond_and_dp_algo(self):
         testcase=[('horse','rose'),('intension','execution')]
         for i in testcase:
-            ond_ans=algo.ond_algo(i[0],i[1])
+            ond_v=algo.my_ond_algo_version1(i[0],i[1])
             dp_ans=algo.dp_algo(i[0],i[1])
-            self.assertEqual(ond_ans,dp_ans,'wops')
+            print(algo.get_edit_script(ond_v))
+            self.assertEqual(algo.get_edit_distance(ond_v),dp_ans,'wops')
     def test(self):
         queryfile=myclass.Fastafile('/test_data/query')
         reffile=myclass.Fastafile('/test_data/ref')
